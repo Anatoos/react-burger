dde3b1f8d3cff6bcbcce990668592663e106ca94
Osipov Anatoliy <osipov.anatoliy@citilink.ru>
2022-09-09 21:47:33 +0300

    Убрал лишний класс заглушку <> там где она не нужна.

--- src/components/ModalOverlay/ModalOverlay.jsx
+++ src/components/ModalOverlay/ModalOverlay.jsx
@@ -3,10 +3,8 @@ import MOStyles from "./ModalOverlay.module.css"
 import PropTypes from "prop-types";
 const ModalOverlay = (props) => {
     return(
-        <>
             <div className={MOStyles.overlay} onClick={() => props.close()}>
             </div>
-        </>
     )
 }
 
