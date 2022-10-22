e2db1ae1b3011d1777db8f046f1929f59394d98b
Osipov Anatoliy <osipov.anatoliy@citilink.ru>
2022-10-23 02:20:36 +0300

    Рефакторинг стилей на composes и верстка register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,30 +1,76 @@
 import React from "react";
-import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Input , Button} from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './register.module.css';
+import { Link } from "react-router-dom";
 
 
 export const Register = () => {
-    const [value, setValue] = React.useState('value')
-    const inputRef = React.useRef(null)
-    const onIconClick = () => {
-        setTimeout(() => inputRef.current.focus(), 0)
+    const [name, setName] = React.useState('Имя');
+    const [email, setEmail] = React.useState('E-mail');
+    const [password, setPassword] = React.useState('Пароль');
+    const [passwordStatus, setPasswordStatus] = React.useState(true);
+    const passwordRef = React.useRef(null);
+    const onPasswordClick = () => {
+        setTimeout(() => passwordRef.current.focus(), 0)
+        setPasswordStatus(!passwordStatus);
+    };
 
-    }
     return(
         <div className={styles.main_block}>
-            <Input
-                type={'text'}
-                placeholder={'placeholder'}
-                onChange={e => setValue(e.target.value)}
-                icon={'CurrencyIcon'}
-                value={value}
-                name={'name'}
-                error={false}
-                ref={inputRef}
-                onIconClick={onIconClick}
-                errorText={'Ошибка'}
-                size={'default'}
-            />
+            <div className={styles.title}>
+                <p>
+                    Регистраци
+                </p>
+            </div>
+            <div className={styles.input}>
+                <Input
+                    type={'text'}
+                    placeholder={'Имя'}
+                    onChange={e => setName(e.target.value)}
+                    value={name}
+                    name={'name'}
+                    error={false}
+                    errorText={'Ошибка'}
+                    size={'default'}
+                />
+            </div>
+            <div className={styles.input}>
+                <Input
+                    type={'email'}
+                    placeholder={'E-mail'}
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
+                    name={'email'}
+                    error={false}
+                    errorText={'Ошибка'}
+                    size={'default'}
+                />
+            </div>
+            <div className={styles.input}>
+                <Input
+                    type={ passwordStatus? 'password' : 'text'}
+                    placeholder={'Пароль'}
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
+                    ref={passwordRef}
+                    name={'password'}
+                    error={false}
+                    errorText={'Ошибка'}
+                    size={'default'}
+                    icon={ passwordStatus ? 'ShowIcon' : 'HideIcon'}
+                    onIconClick={onPasswordClick}
+                />
+            </div>
+            <div className={styles.button}>
+                <Button type="primary" size="large" onClick={register}>
+                    Зарегистрироваться
+                </Button>
+            </div>
+            <div className={styles.registration_block}>
+                <p className="text text_type_main-small">
+                    Уже зарегистрированы? <Link to="/login">Войти</Link>
+                </p>
+            </div>
         </div>
     )
 }
