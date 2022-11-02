6d472496359474dbf5aee201824ee82f9d359489
Osipov Anatoliy <osipov.anatoliy@citilink.ru>
2022-11-02 03:30:41 +0300

    Исправлена работа восстановления пароля

--- src/functions/resetPassword.js
+++ src/functions/resetPassword.js
@@ -1,13 +1,13 @@
 import { API } from "../data/data";
 import { checkResponse } from "./checkResponse";
 
-export const  resetPassword = async (email) => {
+export const  resetPassword = async (emailToReset) => {
     return await fetch( API  + 'password-reset', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(email)
+        body: JSON.stringify(emailToReset)
     }).then(checkResponse)
-        .then(res => res.message === "Password reset sent to mail")
+        .then(res => res.message === "Reset email sent")
         .catch((err) => {
             alert(err ? err : "Unknown error");
             return false;
