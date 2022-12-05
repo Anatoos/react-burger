ca66947977909eb3c615c9b449700f127234c2e7
Osipov Anatoliy <osipov.anatoliy@citilink.ru>
2022-12-06 01:46:52 +0300

    Исправлена ошибка с задвоением модалки фидов.

--- src/pages/ProfileOrderHistory/index.tsx
+++ src/pages/ProfileOrderHistory/index.tsx
@@ -3,11 +3,9 @@ import {useDispatch, useSelector} from "../../types/hooks";
 import styles from "./ProfileOrderHistory.module.css"
 import { TOrder } from "../../types/WSData";
 import { OrderItem } from "./OrderItem";
-import { GET_CURRENT_ORDER_ID, CLEAR_CURRENT_ORDER_ID } from "../../services/actions/currentItem";
-import Modal from "../../components/Modal/Modal";
-import { FeedId } from "../../components/FeedId";
-import { WS_CONNECTION_CLOSED_USER, WS_CONNECTION_START_USER} from "../../services/actions/wsConnectUser";
-import {useLocation, useNavigate} from "react-router-dom";
+import { GET_CURRENT_ORDER_ID } from "../../services/actions/currentItem";
+import { WS_CONNECTION_CLOSED_USER, WS_CONNECTION_START_USER } from "../../services/actions/wsConnectUser";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const  ProfileOrderHistory:FC = () => {
     const navigate = useNavigate();
@@ -21,13 +19,6 @@ export const  ProfileOrderHistory:FC = () => {
     }, [dispatch]);
 
     const orders = useSelector((store)=> store.wsConnectUser.data.orders);
-    const activeModal = useSelector((store) => store.currentItem.currentOrderId)
-    const closeModal = useCallback(()=> {
-        window.history.pushState(null,'','/');
-        dispatch({
-            type: CLEAR_CURRENT_ORDER_ID
-        })
-    },[window.history]);
 
     const setOrderId = useCallback((number)=>{
         dispatch({
@@ -40,11 +31,6 @@ export const  ProfileOrderHistory:FC = () => {
 
     return(
         <div className={styles.mainBlock}>
-            {activeModal && (
-                <Modal title="Детали Заказа" close={closeModal}>
-                    <FeedId/>
-                </Modal>
-            )}
             {orders.map((item:TOrder)=>(
                 <OrderItem key={item._id} elem={item} onClick={setOrderId}/>
             ))}
